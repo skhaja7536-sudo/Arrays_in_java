89a889173d9d56a04adacb373cb02215e57b4714
shaik khaja <skhaja7536@gmail.com>
2026-02-19 22:49:28 +0530

    Implement average calculation for 1D array
    This program calculates the average of a 1D array input by the user.

---
+++ avg_of_1D_array.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the size here: \n");
+	  int a = sc.nextInt();
+	   int[] arr = new int[a];
+	   System.out.println("enter the array values here");
+	  for(int i=0; i<a; i++) {
+	      arr[i] = sc.nextInt();
+	  }
+     
+     System.out.print("Array: [");
+     for(int i =0; i<a; i++){
+         System.out.print(arr[i]);
+         if(i<a-1){
+             System.out.print(", ");
+         }else {
+             System.out.println("]");
+         }
+     }
+     int avg = 0;
+     int sum = 0;
+     for(int i=0; i<a; i++) {
+         sum += arr[i];
+     }
+     avg = sum / a;
+	    System.out.println("Average: " + avg);
+	}
+}

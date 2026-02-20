142a0073159d9e9d7c8589d83bfee3232b729a5d
shaik khaja <skhaja7536@gmail.com>
2026-02-20 11:20:03 +0530

    Implement 3D array input and sum calculation
    This program takes user input to create a 3D array and calculates the sum of each 2D array within it.

---
+++ Sum_of_each_2DArray_in_3DArray.java
@@ -0,0 +1,51 @@
+ import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.println("Hello World");
+		
+		System.out.println("enter depth: ");
+		int a = sc.nextInt();
+		
+		System.out.println("enter no of rows: ");
+		int b =sc.nextInt();
+		
+		System.out.println("enter no.of columns: ");
+		int c = sc.nextInt();
+		
+		int[][][] arr = new int[a][b][c];
+		
+				System.out.println("Taking user inputs: ");
+		for(int i =0; i< a; i++) {
+		    for(int j =0; j<b; j++){
+		        for(int k=0; k<c; k++){
+		           arr[i][j][k] = sc.nextInt();
+		        }
+		    }
+		}
+		
+		
+		System.out.println("3D Array: ");
+		for(int i =0; i< a; i++) {
+		 	System.out.println("Layer " + (i+1) + ":");
+            for(int j =0; j<b; j++){
+		        for(int k=0; k<c; k++){
+		           System.out.print(arr[i][j][k] + " ");
+		        }
+		        System.out.println();
+		    }
+		}
+		
+		for(int i =0; i< a; i++) {
+		    int sum = 0;
+		    for(int j =0; j<b; j++){
+		        for(int k=0; k<c; k++){
+		           sum += arr[i][j][k];
+		        }
+		    }
+		    System.out.println("sum of layer " + (i+1) + ": " + sum);
+		}
+		
+	}
+}
